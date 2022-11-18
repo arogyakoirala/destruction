6e40144e025c5f08b076fb441fdc6c670c83022a
Arogya Koirala <arogya@berkeley.edu>
2022-11-18 13:10:28 -0800

    run.sh and run.sub

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 EXERCISE_ENVIRONMENT="destruction"
 eval "$(conda shell.bash hook)"
 conda activate $EXERCISE_ENVIRONMENT
-python3 destruction-augment.py
-python3 destruction-ready-data-snn.py --balance --pre_image_index 0,1
-python3 destruction-optimize.py 
+python3 -u destruction-augment.py
+python3 -u destruction-ready-data-snn.py --balance --pre_image_index 0,1
+python3 -u destruction-optimize.py 
