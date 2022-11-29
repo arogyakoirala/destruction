cd494252c8ab11850507a62cf5ea7cf67c3984fe
Arogya Koirala <arogya@berkeley.edu>
2022-11-29 13:47:53 -0800

    updated image size

--- experiments/hog_patches/hog_patches.py
+++ experiments/hog_patches/hog_patches.py
@@ -1,6 +1,7 @@
 import zarr
 import numpy as np
 from skimage.feature import hog
+import os
 
 
 CITY = "aleppo_cropped"
@@ -11,6 +12,15 @@ DATA_DIR = "../../../data"
 def read_zarr(city, suffix, path="../data"):
     path = f'{path}/{city}/others/{city}_{suffix}.zarr'
     return zarr.open(path)
+
+def save_zarr(data, city, suffix, path="../data"):
+    path = f'{path}/{city}/others/{city}_{suffix}.zarr'
+    if not os.path.exists(path):
+        zarr.save(path, data)        
+    else:
+        za = zarr.open(path, mode='a')
+        za.append(data)
+
 
 def get_hog(image):
     image = np.float32(image)
@@ -27,7 +37,7 @@ def get_hog_patches(images):
         _, image = get_hog(im)
         image = image.reshape(1, h,w)
         hogs = np.append(hogs, image, axis=0) 
-        print(_.shape)
+        # print(_.shape)
 
 
 
@@ -35,5 +45,18 @@ def get_hog_patches(images):
 
 
 images_tr = read_zarr('aleppo_cropped', 'im_tr', DATA_DIR)[:]
-get_hog_patches(images_tr)
+images_tr_hog = get_hog_patches(images_tr)
+save_zarr(city=CITY, data=images_tr_hog, suffix="hog_tr", path=DATA_DIR)
+
+
+images_va = read_zarr('aleppo_cropped', 'im_va', DATA_DIR)[:]
+images_va_hog = get_hog_patches(images_va)
+save_zarr(city=CITY, data=images_va_hog, suffix="hog_va", path=DATA_DIR)
+
+
+images_te = read_zarr('aleppo_cropped', 'im_te', DATA_DIR)[:]
+images_te_hog = get_hog_patches(images_te)
+save_zarr(city=CITY, data=images_te_hog, suffix="hog_te", path=DATA_DIR)
+
+
 # hv(images_tr)
