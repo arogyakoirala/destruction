32a4761cb51c4942bda3e661914afe8f2222a13d
Arogya Koirala <arogya@berkeley.edu>
2022-12-01 13:19:27 -0800

    get dist

--- experiments/testing/test.py
+++ experiments/testing/test.py
@@ -5,31 +5,31 @@ copy = "aleppo_damage_col_drop.gpkg"
 orig = "aleppo_damage.gpkg"
 
 
-copy = gpd.read_file(copy)
-orig = gpd.read_file(orig)
+# copy = gpd.read_file(copy)
+# orig = gpd.read_file(orig)
 
 
-print(orig.describe())
-print(copy.describe())
-
-
-print(len(orig), len(copy))
+# print(orig.describe())
 # print(copy.describe())
 
 
-CITY = 'aleppo_cropped'
-SUFFIX = 'hog'
-BANDS = 1
-N_BANDS = 3
-DATA_DIR = "../../../data"
-MODEL_DIR = "../../../models"
-BATCH_SIZE = 32
-PATCH_SIZE = (128,128)
-FILTERS = [32]
-DROPOUT = [0.3, 0.45]
-EPOCHS = [70, 100]
-UNITS = [64]
-LR = [0.003, 0.003, 0.004]
+# print(len(orig), len(copy))
+# # print(copy.describe())
+
+
+# CITY = 'aleppo_cropped'
+# SUFFIX = 'hog'
+# BANDS = 1
+# N_BANDS = 3
+# DATA_DIR = "../../../data"
+# MODEL_DIR = "../../../models"
+# BATCH_SIZE = 32
+# PATCH_SIZE = (128,128)
+# FILTERS = [32]
+# DROPOUT = [0.3, 0.45]
+# EPOCHS = [70, 100]
+# UNITS = [64]
+# LR = [0.003, 0.003, 0.004]
 
 
 def read_zarr(city, suffix, path="../data"):
