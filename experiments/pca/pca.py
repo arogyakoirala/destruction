71d8ca2f01b797bbe7094e62f3c9f633840202a6
Arogya Koirala <arogya@berkeley.edu>
2022-12-01 01:50:38 -0800

    Updated PCA Code

--- experiments/pca/pca.py
+++ experiments/pca/pca.py
@@ -6,7 +6,7 @@ args = parser.parse_args()
 SUFFIX = 'hog'
 CITY = 'aleppo_cropped'
 DATA_DIR = "../../../data"
-PERC_VARIANCE = 0.75
+PERC_VARIANCE = 0.80
 
 if args.suffix:
     SUFFIX = args.suffix 
@@ -53,21 +53,36 @@ def delete_zarr_if_exists(city, suffix, path="../data"):
     if os.path.exists(path):
         shutil.rmtree(path)
 
-suffixes = [f"pca_{SUFFIX}_tr",f"pca_{SUFFIX}_va",f"pca_{SUFFIX}_te"]
+suffixes = [f"pca_{SUFFIX}_tr_pre",f"pca_{SUFFIX}_va_pre",f"pca_{SUFFIX}_te_pre", f"pca_{SUFFIX}_tr_post",f"pca_{SUFFIX}_va_post",f"pca_{SUFFIX}_te_post"]
 for s in suffixes:
     delete_zarr_if_exists(CITY, s, DATA_DIR)
 
 
-images_tr = read_zarr(CITY, f"{SUFFIX}_tr_post", DATA_DIR)[:]
-images_va = read_zarr(CITY, f"{SUFFIX}_va_post", DATA_DIR)[:]
-images_te = read_zarr(CITY, f"{SUFFIX}_te_post", DATA_DIR)[:]
+images_pre = read_zarr(CITY, f"{SUFFIX}_tr_pre", DATA_DIR)[:]
+n_pre = images_pre.shape
+new_array = [tuple(row) for row in images_pre]
+images_pre = np.unique(new_array, axis=0)
+images_pre = images_pre.reshape(images_pre.shape[0], 128*128)
+del new_array
+print(f"images_pre shape: {images_pre.shape}")
 
-images_tr = images_tr.reshape((images_tr.shape[0], 128*128))
-images_va = images_va.reshape((images_va.shape[0], 128*128))
-images_te = images_te.reshape((images_te.shape[0], 128*128))
+images_tr_pre = read_zarr(CITY, f"{SUFFIX}_tr_pre", DATA_DIR)[:]
+images_va_pre = read_zarr(CITY, f"{SUFFIX}_va_pre", DATA_DIR)[:]
+images_te_pre = read_zarr(CITY, f"{SUFFIX}_te_pre", DATA_DIR)[:]
+images_tr_post = read_zarr(CITY, f"{SUFFIX}_tr_post", DATA_DIR)[:]
+images_va_post = read_zarr(CITY, f"{SUFFIX}_va_post", DATA_DIR)[:]
+images_te_post = read_zarr(CITY, f"{SUFFIX}_te_post", DATA_DIR)[:]
 
-images_all = np.append(images_tr, images_va, axis=0)
-images_all = np.append(images_all, images_te, axis=0)
+images_tr_pre = images_tr_pre.reshape((images_tr_pre.shape[0], 128*128))
+images_va_pre = images_va_pre.reshape((images_va_pre.shape[0], 128*128))
+images_te_pre = images_te_pre.reshape((images_te_pre.shape[0], 128*128))
+images_tr_post = images_tr_post.reshape((images_tr_post.shape[0], 128*128))
+images_va_post = images_va_post.reshape((images_va_post.shape[0], 128*128))
+images_te_post = images_te_post.reshape((images_te_post.shape[0], 128*128))
+
+images_all = np.append(images_tr_post, images_va_post, axis=0)
+images_all = np.append(images_all, images_te_post, axis=0)
+images_all = np.append(images_all, images_pre, axis=0)
 
 
 
@@ -77,9 +92,6 @@ images_all = images_all.reshape((images_all.shape[0], 128*128))
 #     n, h, w = images_tr.shape
 #     images = images_tr.reshape(n, h*w)
 
-print(images_tr.shape)
-
-
 
 images_std, mu, sigma = standardize(images_all, full=True)
 # print(images_std.shape)
@@ -87,7 +99,7 @@ k=min(len(images_std), 500)
 pca = PCA(n_components=k)
 pca.fit(images_std)
 n, perc = min(enumerate(pca.explained_variance_ratio_.cumsum()), key=lambda x: abs(x[1]-PERC_VARIANCE))
-print(f"{n} principal components explain ~{100*perc.astype(int)}% of variance")
+print(f"{n} principal components explain ~{(100*perc).astype(int)}% of variance")
 
 fig, ax = plt.subplots(1,1,figsize=(15,3), dpi=200)
 sns.lineplot(x=range(1, k+1), y=pca.explained_variance_ratio_.cumsum(), ax=ax);
@@ -100,32 +112,47 @@ plt.savefig(f"{SUFFIX}_pca_var_plot.png")
 
 
 
-images_tr = standardize(images_tr, mu, sigma)
-pcas = pca.transform(images_tr)
+images_tr_pre = standardize(images_tr_pre, mu, sigma)
+pcas = pca.transform(images_tr_pre)
 pcas[:, -(k-n):] = 0
-save_zarr(city=CITY, data=pcas, suffix=f"pca_{SUFFIX}_tr", path=DATA_DIR)
+save_zarr(city=CITY, data=pcas, suffix=f"pca_{SUFFIX}_tr_pre", path=DATA_DIR)
 
-images_va = standardize(images_va, mu, sigma)
-pcas = pca.transform(images_va)
+images_va_pre = standardize(images_va_pre, mu, sigma)
+pcas = pca.transform(images_va_pre)
 pcas[:, -(k-n):] = 0
-save_zarr(city=CITY, data=pcas, suffix=f"pca_{SUFFIX}_va", path=DATA_DIR)
+save_zarr(city=CITY, data=pcas, suffix=f"pca_{SUFFIX}_va_pre", path=DATA_DIR)
 
-images_te = standardize(images_te, mu, sigma)
-pcas = pca.transform(images_te)
+images_te_pre = standardize(images_te_pre, mu, sigma)
+pcas = pca.transform(images_te_pre)
 pcas[:, -(k-n):] = 0
-save_zarr(city=CITY, data=pcas, suffix=f"pca_{SUFFIX}_te", path=DATA_DIR)
+save_zarr(city=CITY, data=pcas, suffix=f"pca_{SUFFIX}_te_pre", path=DATA_DIR)
+
+images_tr_post = standardize(images_tr_post, mu, sigma)
+pcas = pca.transform(images_tr_post)
+pcas[:, -(k-n):] = 0
+save_zarr(city=CITY, data=pcas, suffix=f"pca_{SUFFIX}_tr_post", path=DATA_DIR)
+
+images_va_post = standardize(images_va_post, mu, sigma)
+pcas = pca.transform(images_va_post)
+pcas[:, -(k-n):] = 0
+save_zarr(city=CITY, data=pcas, suffix=f"pca_{SUFFIX}_va_post", path=DATA_DIR)
+
+images_te_post = standardize(images_te_post, mu, sigma)
+pcas = pca.transform(images_te_post)
+pcas[:, -(k-n):] = 0
+save_zarr(city=CITY, data=pcas, suffix=f"pca_{SUFFIX}_te_post", path=DATA_DIR)
 
 fig,ax = plt.subplots(2,5,sharex=True,sharey=True,figsize=(8,3), dpi=300)
 axes = ax.flatten()
-pcas = pca.transform(images_va[:5])
+pcas = pca.transform(images_va_post[:5])
 pcas[:, -(k-n):] = 0
 for i, im in enumerate(pcas):
     result = pca.inverse_transform(im).reshape((128,128))
-    axes[i].imshow(images_va[i].reshape(128,128), cmap="gray")
+    axes[i].imshow(images_va_post[i].reshape(128,128), cmap="gray")
     axes[i+5].imshow(result, cmap="gray")
 for ax in axes:
     ax.set_xticks([])
     ax.set_yticks([])
-fig.suptitle(f"Images reconstructed using {n} principal components; {PERC_VARIANCE*100}% variance retained",fontsize=12)
+fig.suptitle(f"Reconstr. with {n} principal components; explains ~{(100*perc).astype(int)}% of variance",fontsize=12)
 plt.tight_layout()
 plt.savefig(f"{SUFFIX}_reconstructed.png")
