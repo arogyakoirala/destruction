f19b08d2792450803aa3d1bbf5a5b1ba75edb195
Arogya Koirala <arogyak@Arogyas-MacBook-Air.local>
2022-12-30 13:59:53 +0545

    updated tile logic

--- prep_data.sh
+++ prep_data.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 source /usr/local/anaconda3/condabin/conda
-conda activate mlp
+conda activate des-linux
 
-declare -a Cities=("aleppo" "damascus" "daraa" "deir-ez-zor" "hama" "homs" "idlib")
+declare -a Cities=("aleppo" "damascus" "daraa" "deir-ez-zor" "hama" "homs" "idlib" "raqqa")
 
 for city in "${Cities[@]}"; do
+    echo "Sampling:" $city
     python sample.py --city $city
+    echo "Labeling:" $city
     python label.py --city $city
+    echo "Tiling:" $city
     python tile.py --city $city
+    echo "Balancing:" $city
     python balance.py --city $city
+    echo "Shuffling:" $city
     python shuffle.py --city $city
 done
