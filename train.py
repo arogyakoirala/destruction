38511993a1c857e4dea3ad07cb7c4c809197b97a
Arogya Koirala <arogyak@Arogyas-MacBook-Air.local>
2023-01-01 19:37:09 +0545

    Switch to Siamese

--- train.py
+++ train.py
@@ -178,12 +178,15 @@ def dense_block(inputs, units:int=1, dropout:float=0, name:str=''):
 #     return tensor
 
 def convolution_block(inputs, filters:int, dropout:float, name:str):
-    convolution   = layers.Conv2D(filters=filters, kernel_size=(3, 3), activation='relu', padding='same', use_bias=False, kernel_initializer='he_normal', name=f'{name}_convolution')(inputs)
-    pooling       = layers.MaxPool2D(pool_size=(2, 2), name=f'{name}_pooling')(convolution)
-    normalisation = layers.BatchNormalization(name=f'{name}_normalisation')(pooling)
-    outputs       = layers.SpatialDropout2D(rate=dropout, name=f'{name}_dropout')(normalisation)
-    return outputs
-
+    tensor = layers.Conv2D(filters=filters, kernel_size=(3, 3), padding='same', use_bias=False, kernel_initializer='he_normal', name=f'{name}_convolution1')(inputs)
+    tensor = layers.Activation('relu', name=f'{name}_activation1')(tensor)
+    tensor = layers.BatchNormalization(name=f'{name}_normalisation1')(tensor)
+    tensor = layers.Conv2D(filters=filters, kernel_size=(3, 3), padding='same', use_bias=False, kernel_initializer='he_normal', name=f'{name}_convolution2')(tensor)
+    tensor = layers.Activation('relu', name=f'{name}_activation2')(tensor)
+    tensor = layers.BatchNormalization(name=f'{name}_normalisation2')(tensor)
+    tensor = layers.MaxPool2D(pool_size=(2, 2), name=f'{name}_pooling')(tensor)
+    tensor = layers.SpatialDropout2D(rate=dropout, name=f'{name}_dropout')(tensor)
+    return tensor
 
 def distance_layer(inputs):
     input0, input1 = inputs
@@ -201,6 +204,17 @@ def encoder_block_separated(inputs, filters:int=1, dropout=0, name:str=''):
     tensor  = convolution_block(tensor, filters=filters*5, dropout=dropout, name=f'{name}_block5')
     outputs = layers.Flatten(name=f'{name}_flatten')(tensor)
     return outputs
+
+def encoder_block_shared(shape:tuple, filters:int=1, dropout=0):
+    inputs  = layers.Input(shape=shape, name='inputs')
+    tensor  = convolution_block(inputs, filters=filters*1, dropout=dropout, name='block1')
+    tensor  = convolution_block(tensor, filters=filters*2, dropout=dropout, name='block2')
+    tensor  = convolution_block(tensor, filters=filters*3, dropout=dropout, name='block3')
+    tensor  = convolution_block(tensor, filters=filters*4, dropout=dropout, name='block4')
+    tensor  = convolution_block(tensor, filters=filters*5, dropout=dropout, name='block5')
+    outputs = layers.GlobalAveragePooling2D(name='global_pooling')(tensor)
+    encoder = models.Model(inputs=inputs, outputs=outputs, name='encoder')
+    return encoder
 
 
 def siamese_convolutional_network(shape:tuple, args_encode:dict, args_dense:dict):
