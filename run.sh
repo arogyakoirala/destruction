84275fe3258a416bb0c20bd9efd9be2a5d205730
Arogya Koirala <arogyak@Arogyas-MacBook-Air.local>
2023-02-10 15:44:58 -0800

    Updated run.sh to just have code from prep_data.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,31 @@
 EXERCISE_ENVIRONMENT="env"
 eval "$(conda shell.bash hook)"
 conda activate $EXERCISE_ENVIRONMENT
-./prep_data.sh
+# ./prep_data.sh
 # python3 -u destruction-augment.py
 # python3 -u destruction-ready-data-snn.py --balance --pre_image_index 0,1
 # python3 -u destruction-optimize.py 
+
+
+
+declare -a Cities=("aleppo" "damascus" "daraa" "deir-ez-zor" "hama" "homs" "idlib" "raqqa")
+# declare -a Cities=("aleppo" "daraa")
+# declare -a Cities=("raqqa")
+
+declare -a data_dir=$1
+echo "Data Dir: $data_dir";
+
+for city in "${Cities[@]}"; do
+    echo "Sampling:" $city
+    python sample.py --city $city --data_dir $data_dir
+    echo "Labeling:" $city
+    python label.py --city $city --data_dir $data_dir
+    echo "Tiling:" $city
+    python tile.py --city $city --data_dir $data_dir
+    echo "Balancing:" $city
+    python balance.py --city $city --data_dir $data_dir
+    echo "Shuffling:" $city
+    python shuffle.py --city $city --data_dir $data_dir
+    echo "Shuffling again:" $city
+    python shuffle.py --city $city --data_dir $data_dir
+done
