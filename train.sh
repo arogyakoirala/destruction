63de8057cd6bffc9fe0f56fd8b0533de069a1d63
Arogya Koirala <arogyak@Arogyas-MacBook-Air.local>
2023-02-10 16:10:23 -0800

    updated train.sh

--- train.sh
+++ train.sh
@@ -3,4 +3,4 @@ EXERCISE_ENVIRONMENT="env"
 eval "$(conda shell.bash hook)"
 conda activate $EXERCISE_ENVIRONMENT
 
-python3 -u train.py --cities aleppo,damascus,daraa,deir-ez-zor,hama,homs,idlib,raqqa --model double --data_dir /lustre/ific.uv.es/ml/iae091/data --output_dir /lustre/ific.uv.es/ml/iae091/outputs
+python -u train.py --cities aleppo,damascus,daraa,deir-ez-zor,hama,homs,idlib,raqqa --model double --data_dir /lustre/ific.uv.es/ml/iae091/data --output_dir /lustre/ific.uv.es/ml/iae091/outputs
