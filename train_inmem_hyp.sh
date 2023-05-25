395a13dbede94849ad2c682a42f7777b83cc9316
Arogya Koirala <arogyakoirala@gmail.com>
2023-05-25 15:06:52 -0700

    Update train_inmem_hyp.sh

--- train_inmem_hyp.sh
+++ train_inmem_hyp.sh
@@ -16,7 +16,7 @@ for city in "${Cities[@]}"; do
             for lr in "${LearningRates[@]}"; do
                 for units in "${Units[@]}"; do
                     for filters in "${Filters[@]}"; do
-                        python -u train_inmem_hyp.py --cities $city --model double --filters $filters --units $units --lr $lr --batch_size $batchsize --dropout $dropout --city $city --data_dir /lustre/ific.uv.es/ml/iae091/data --output_dir /lustre/ific.uv.es/ml/iae091/outputs
+                        python -u train_inmem_hyp.py --cities $city --model double --filters $filters --units $units --lr $lr --batch_size $batchsize --dropout $dropout --data_dir /lustre/ific.uv.es/ml/iae091/data --output_dir /lustre/ific.uv.es/ml/iae091/outputs
                     done
                 done
             done
