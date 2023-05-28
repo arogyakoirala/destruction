d28dbfb764ea4de09d671c9f6bc822eb65947179
Arogya Koirala <arogyakoirala@gmail.com>
2023-05-28 13:58:19 -0700

    attempt to make it run

--- train_inmem.sh
+++ train_inmem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-EXERCISE_ENVIRONMENT="env"
-eval "$(conda shell.bash hook)"
-conda activate $EXERCISE_ENVIRONMENT
+# EXERCISE_ENVIRONMENT="env"
+# eval "$(conda shell.bash hook)"
+# conda activate $EXERCISE_ENVIRONMENT
 
 python -u train_inmem.py --cities $1 --model double --data_dir /lustre/ific.uv.es/ml/iae091/data --output_dir /lustre/ific.uv.es/ml/iae091/outputs
